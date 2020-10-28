234b62314f41c1286239ca9b0e12ea28a12f5dac
Xavier Cooper <x@xavier.codes>
2020-10-28 15:15:01 -0400

    enabling enqueueing of parent theme styles and scripts

--- functions.php
+++ functions.php
@@ -4,11 +4,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 function understrap_remove_scripts() {
-    wp_dequeue_style( 'understrap-styles' );
+    /*wp_dequeue_style( 'understrap-styles' );
     wp_deregister_style( 'understrap-styles' );
 
-    wp_dequeue_script( 'understrap-scripts' );
-    wp_deregister_script( 'understrap-scripts' );
+    wp_dequeue_script( 'understrap-scripts' )
+        wp_deregister_script( 'understrap-scripts' );*/
 
     // Removes the parent themes stylesheet and scripts from inc/enqueue.php
 }
