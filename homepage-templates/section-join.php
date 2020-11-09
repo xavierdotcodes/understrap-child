ebf938c04599f4f1c64bd7faf9bcfbaecf672ee7
Xavier Cooper <x@xavier.codes>
2020-11-08 21:36:18 -0500

    modifying join section template to better accomodate styles

--- homepage-templates/section-join.php
+++ homepage-templates/section-join.php
@@ -2,9 +2,10 @@
 wp_reset_query();
 $background_image = get_field( 'join_now_section_background_image' ); 
  ?>
-<section id="joinnow-section" class="container" style="background-image:url('<?php echo $background_image; ?> '); background-size:cover; background-repeat:no-repeat;">
+<section id="joinnow-section" class="container" style="background-image:url('<?php echo $background_image; ?> ');">
     <div class="row">
-        <div class="col-md-12">
+        <div class="col-md-6">
+            <h2 class="section-heading" >Join my newsletter and receive<br/> more English lessons for FREE</h2>
 
 
 
@@ -22,7 +23,7 @@ $options = array( 'post_id' => 'new_post',
                     'updated_message' => __( 'Your email has been submitted. Thank You!', 'acf' ),
                     'field_groups' => array('group_5fa6d09b86c75'), 
                     'label_placement' => 'left', 
-                    'html_submit_button' => '<input type="submit" class="acf-button button rounded-pill radioflyer button-large col-md-2" id="join-form-submit-button" value="%s" />',
+                    'html_submit_button' => '<input type="submit" class="acf-button button rounded-pill radioflyer button-large" id="join-form-submit-button" value="%s" />',
 );
 
 acf_form( $options ); 
