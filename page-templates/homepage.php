6d40b7eaaee73342376a4298662ba5945ade083d
Xavier Cooper <x@xavier.codes>
2020-11-08 20:18:47 -0500

    Add all template parts for the homepage.

--- page-templates/homepage.php
+++ page-templates/homepage.php
@@ -10,45 +10,41 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
+acf_form_head();
 get_header();
 $container = get_theme_mod( 'understrap_container_type' );
 
 ?>
 
-<div class="wrapper" id="full-width-page-wrapper">
+    <div class="wrapper" id="full-width-page-wrapper">
 
-	<div class="<?php echo esc_attr( $container ); ?>" id="content">
+        <div class="<?php echo esc_attr( $container ); ?>" id="content">
 
-		<div class="row">
+            <div class="row">
 
-            <div class="col-md-12 content-area" id="primary">
+                <div class="col-md-12 content-area" id="primary">
 
-				<main class="site-main" id="main" role="main">
-
-					<?php
-                    if ( is_front_page() ) {
-                        get_template_part( 'global-templates/hero' );
-                    }
-					while ( have_posts() ) {
-						the_post();
-						get_template_part( 'loop-templates/content', 'page' );
-
-						// If comments are open or we have at least one comment, load up the comment template.
-						if ( comments_open() || get_comments_number() ) {
-							comments_template();
-						}
-					}
-					?>
-
-				</main><!-- #main -->
-
-			</div><!-- #primary -->
-
-		</div><!-- .row end -->
-
-	</div><!-- #content -->
-
-</div><!-- #full-width-page-wrapper -->
+                    <main class="site-main" id="main" role="main">
 
 <?php
-get_footer();
+    get_template_part( 'global-templates/hero' );
+    get_template_part( 'homepage-templates/section', 'join' ); 
+    get_template_part( 'homepage-templates/section', 'logos' );
+    get_template_part( 'homepage-templates/section', 'about' );
+    get_template_part( 'homepage-templates/section', 'resources' );
+    get_template_part( 'homepage-templates/section', 'lessons' ); 
+    get_template_part( 'homepage-templates/section', 'connect' );
+?>
+
+
+                    </main><!-- #main -->		
+
+                </div><!-- #primary -->
+
+            </div><!-- .row end -->
+
+        </div><!-- #content -->
+
+    </div><!-- #full-width-page-wrapper -->
+
+<?php get_footer();  ?>
