ab7a96ec5dc32427d59f97cf9856494fa2da3026
Xavier Cooper <x@xavier.codes>
2020-11-11 10:49:11 -0500

    commiting changes made to homepage template

--- page-templates/homepage.php
+++ page-templates/homepage.php
@@ -16,13 +16,13 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 ?>
 
-    <div class="wrapper" id="full-width-page-wrapper">
+    <div class="wrapper py-0" id="full-width-page-wrapper">
 
-        <div class="<?php echo esc_attr( $container ); ?>" id="content">
+        <div class="<?php echo esc_attr( $container ); ?> p-0" id="content">
 
             <div class="row">
 
-                <div class="col-md-12 content-area" id="primary">
+                <div class="col-12 content-area" id="primary">
 
                     <main class="site-main" id="main" role="main">
 
